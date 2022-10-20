2cfcdbe80f8c838c7a666ed28ffff5e3b4356727
abstergo <ts6909606@gmail.com>
2022-10-20 14:52:16 -0400

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/adaptive.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="header_icon">
+            </div>
+            <div class="hidden_button">
+            </div>
+            <div class="hidden_section">
+            <div class="header_el"><a href="">
+                    Вся коллекция
+                </a></div>
+                <div class="header_el"><a href="">
+                    Новинки
+                </a></div>
+                <div class="header_el"><a href="">
+                    Доставка
+                </a></div>
+                <div class="header_el"><a href="">
+                    Контакты
+                </a></div>
+                <div class="header_el"><a href="">
+                    О нас
+                </a></div>
+            </div>
+            <div class="section">
+                <div class="header_el"><a href="">
+                    Вся коллекция
+                </a></div>
+                <div class="header_el"><a href="">
+                    Новинки
+                </a></div>
+                <div class="header_el"><a href="">
+                    Доставка
+                </a></div>
+                <div class="header_el"><a href="">
+                    Контакты
+                </a></div>
+                <div class="header_el"><a href="">
+                    О нас
+                </a></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
